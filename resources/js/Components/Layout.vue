f51b5b8633d1f299e39c1dad35567b754fb73e6b
Victor Gonzalez <vgonzalez@cendrassos.net>
2024-05-31 22:54:28 +0200

    Landing page done

--- resources/js/Components/Layout.vue
+++ resources/js/Components/Layout.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue';
 
 const drawerNavigation = ref(false);
 
-const logoUrl = '/storage/images/logo.svg';
+const logoUrl = '/images/logo.svg';
 
 const toggleDrawer = () => {
     drawerNavigation.value = !drawerNavigation.value;
@@ -21,7 +21,7 @@ const handleMainClick = () => {
         <nav class="flex flex-row w-full items-center justify-between p-3 z-50 background border-b border-gray-400">
 
             <div class="flex justify-center align-center accent rounded">
-                <button class="w-10 h-10 relative" @click.prevent="toggleDrawer">
+                <button class="w-10 h-10 relative" @click.prevent="toggleDrawer" aria-label="Sidebar">
                     <div class="block w-5 absolute left-4 transform -translate-x-1/2 -translate-y-1/2">
                         <span class="block absolute h-0.5 w-7 bg-current transform transition duration-500 ease-in-out"
                             :class="{ 'rotate-45': drawerNavigation, ' -translate-y-1.5': !drawerNavigation }"></span>
@@ -44,12 +44,12 @@ const handleMainClick = () => {
         </nav>
 
         <div class="flex flex-row w-full h-full relative background">
-            <aside v-if="drawerNavigation" class="flex flex-col h-full p-3 w-2/3 md:w-1/3 lg:w-1/6 z-50 secondary border-r border-gray-400">
+            <aside v-if="drawerNavigation" class="flex absolute text-lg flex-col h-full p-3 w-2/3 md:w-1/3 lg:w-1/6 z-50 secondary border-r border-gray-400">
                 <a href="/" class="mb-2">Inici</a>
                 <a href="/aboutUs">Sobre Nosaltres</a>
             </aside>
 
-            <main class="w-full h-full" @click="handleMainClick">
+            <main class="w-full h-full overflow-auto" @click="handleMainClick">
                 <slot></slot>
                 <div v-if="drawerNavigation" class="fixed inset-0 bg-black opacity-20 z-10"></div>
             </main>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,7 +1,22 @@
 <template>
-    <Layout class="w-full h-full">
-        <div class="w-full h-1/2 p-3"> THIS IS A TEST FOR THE MAIN VIEW OF THE PAGE </div>
-        <div class="w-full h-1/2 p-3"> THIS IS A TEST FOR THE MAIN VIEW OF THE PAGE </div>
+    <Layout class="flex flex-col w-full h-full">
+        <div class="flex flex-col items-center w-full h-1/3 p-3">
+            <div class="flex flex-col items-center text-center">
+                <h1 class="text-3xl font-semibold">Benvingut a Forquilletes</h1>
+                <p class="text-lg">Preparat per descobrir noves experiències?</p>
+                <p class="text-lg">Potser busques que el teu negoci sigui vist?</p>
+                <p class="text-lg">O vols compartir la teva experiència amb altres usuaris?</p>
+                <h2 class="text-2xl font-semibold">Si la teva resposta és positiva, benvingut!</h2>
+            </div>
+        </div>
+        <div class="flex flex-col items-center w-full h-1/2 p-3">
+            <div class="flex flex-col items-center text-center">
+                <h1 class="text-3xl font-semibold">Una llista de restaurants que s'actualitza amb els usuaris</h1>
+                <p class="text-lg">Evitaràs tornar-te boig buscant el menú</p>
+                <p class="text-lg">Fotos i opinions per als més escrupolosos</p>
+                <p class="text-lg">No et preocupis si no te'n recordes dels carrers, som el teu mapa!</p>
+            </div>
+        </div>
     </Layout>
 </template>
 
